72d79cd4969ffc154bfc1769527da6d38d988185
Alexander Hakki <ahakki@ahakki.xyz>
2015-10-28 17:46:25 +0100

    problem 31 progress

--- ProjectEuler/Problem031.hs
+++ ProjectEuler/Problem031.hs
@@ -4,5 +4,20 @@ import Ahakki.Resis
 
 result:: PEResult
 result = NotComplete
-coins :: [Integer]
-coins = [1, 2, 5, 10, 20, 50, 100, 200]
+
+
+data Coin = Coin {  value     :: Integer
+                 ,  contents  :: [Coin]}
+
+instance Show Coin where
+  show (Coin v c) = unwords [show v, "pence, can be made of:", show (fmap value c)]
+
+
+p1    = Coin 1    []
+p2    = Coin 2    [p1, p1]
+p5    = Coin 5    [p1, p2, p2]
+p10   = Coin 10   [p5, p5]
+p20   = Coin 20   [p10, p10]
+p50   = Coin 50   [p20, p20, p10]
+p100  = Coin 100  [p50, p50]
+p200  = Coin 200  [p100, p100]
