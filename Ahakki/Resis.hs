36d6ce0654aae9b509879431285697d63cbf9808
Alexander Hakki <ahakki@ahakki.xyz>
2015-10-28 15:49:08 +0100

    refined PEResult datatype

--- Ahakki/Resis.hs
+++ Ahakki/Resis.hs
@@ -2,9 +2,10 @@ module Ahakki.Resis where
 
 import Data.List
 
-data PEResult =     PEResult { peProblemNum     :: Integer
-                             , peProblemAnswer  :: Integer}
-                 |  NotComplete
+data PEResult =    PEResult { peProblemNum     :: Integer
+                            , peProblemAnswer  :: Integer}
+                 | NotComplete
+
 
 instance Show (PEResult) where
     show (PEResult n a) = unwords ["Result of Project Euler Problem Number",  show n, "is", show a]
